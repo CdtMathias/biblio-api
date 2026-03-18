af7e1e5d8c922438e5551d42ca7c436a1f7fd7f2
CdtMathias <50462130+CdtMathias@users.noreply.github.com>
2026-03-18 14:46:26 +0100

    Update book_model.py

--- models/book_model.py
+++ models/book_model.py
@@ -1,5 +1,5 @@
 class Book:
-    def __init__(self, id, title, author, state, year_published):
+    def __init__(self, id, title, author, year_published, state):
         self.id = id
         self.title = title
         self.author = author
